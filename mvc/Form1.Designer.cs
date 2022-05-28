9ff90675f9eef51aa6aff92cd88e9b2d2613d34e
unknown <vlad_lcd@mail.ru>
2022-05-28 12:54:55 +0300

    Создан проект MVC.

---
+++ mvc/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace projectsamozachet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dish = new System.Windows.Forms.Button();
+            this.ingredient = new System.Windows.Forms.Button();
+            this.product = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dish
+            // 
+            this.dish.BackColor = System.Drawing.Color.White;
+            this.dish.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.dish.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dish.Location = new System.Drawing.Point(16, 123);
+            this.dish.Name = "dish";
+            this.dish.Size = new System.Drawing.Size(488, 100);
+            this.dish.TabIndex = 2;
+            this.dish.Text = "Dish";
+            this.dish.UseVisualStyleBackColor = false;
+            this.dish.Click += new System.EventHandler(this.dish_Click);
+            // 
+            // ingredient
+            // 
+            this.ingredient.BackColor = System.Drawing.Color.White;
+            this.ingredient.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.ingredient.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ingredient.Location = new System.Drawing.Point(16, 229);
+            this.ingredient.Name = "ingredient";
+            this.ingredient.Size = new System.Drawing.Size(488, 100);
+            this.ingredient.TabIndex = 3;
+            this.ingredient.Text = "Ingredient";
+            this.ingredient.UseVisualStyleBackColor = false;
+            this.ingredient.Click += new System.EventHandler(this.ingredient_Click);
+            // 
+            // product
+            // 
+            this.product.BackColor = System.Drawing.Color.White;
+            this.product.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.product.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.product.Location = new System.Drawing.Point(16, 17);
+            this.product.Name = "product";
+            this.product.Size = new System.Drawing.Size(488, 100);
+            this.product.TabIndex = 4;
+            this.product.Text = "Product";
+            this.product.UseVisualStyleBackColor = false;
+            this.product.Click += new System.EventHandler(this.product_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 352);
+            this.Controls.Add(this.product);
+            this.Controls.Add(this.ingredient);
+            this.Controls.Add(this.dish);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Restaurant";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button dish;
+        private System.Windows.Forms.Button ingredient;
+        private System.Windows.Forms.Button product;
+    }
+}
+
---
+++ mvc/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace projectsamozachet
+{
+    public partial class Form1 : Form
+    {
+        Facade facade;
+        MySqlConnection connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.facade = new Facade();
+            this.connection = new MySqlConnection("server=127.0.0.1;username=root;password=root;database=restaurant");
+            connect();
+        }
+
+        public Form1(Facade facade)
+        {
+            InitializeComponent();
+
+            this.facade = facade;
+            this.connection = new MySqlConnection("server=127.0.0.1;username=root;password=root;database=restaurant");
+        }
+
+        private void ingredient_Click(object sender, EventArgs e)
+        {
+            Ingredient ingredient = new Ingredient(facade, this);
+            ingredient.Show();
+            Hide();
+        }
+
+        private void dish_Click(object sender, EventArgs e)
+        {
+            Dish dish = new Dish(facade, this);
+            dish.Show();
+            Hide();
+        }
+
+        private void product_Click(object sender, EventArgs e)
+        {
+            Product product = new Product(facade, this);
+            product.Show();
+            Hide();
+        }
+
+        void connect()
+        {
+            string query = "SELECT * FROM product";
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection);
+            adapter.Fill(table);
+            foreach (DataRow row in table.Rows)
+                facade.create_product_list(row.ItemArray);
+            query = "SELECT * FROM dish";
+            table = new DataTable();
+            adapter = new MySqlDataAdapter(query, connection);
+            adapter.Fill(table);
+            foreach (DataRow row in table.Rows)
+                facade.create_dish_list(row.ItemArray);
+            query = "SELECT * FROM ingredient";
+            table = new DataTable();
+            adapter = new MySqlDataAdapter(query, connection);
+            adapter.Fill(table);
+            foreach (DataRow row in table.Rows)
+                facade.create_ingredient_list(row.ItemArray);
+        }
+    }
+}
