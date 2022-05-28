61814d39d1803d071a5c6db10cc760a7d39f877b
unknown <vlad_lcd@mail.ru>
2022-05-28 14:49:17 +0300

    Добавлен Product.

---
+++ mvc/Product.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace projectsamozachet
+{
+    partial class Product
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgv_product = new System.Windows.Forms.DataGridView();
+            this.tb_id_product = new System.Windows.Forms.TextBox();
+            this.tb_name_product = new System.Windows.Forms.TextBox();
+            this.tb_price_1kg_rub = new System.Windows.Forms.TextBox();
+            this.btn_insert = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.execute_info = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_product)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgv_product
+            // 
+            this.dgv_product.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv_product.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv_product.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_product.Location = new System.Drawing.Point(3, 3);
+            this.dgv_product.Name = "dgv_product";
+            this.dgv_product.RowHeadersWidth = 51;
+            this.dgv_product.RowTemplate.Height = 24;
+            this.dgv_product.Size = new System.Drawing.Size(753, 286);
+            this.dgv_product.TabIndex = 0;
+            // 
+            // tb_id_product
+            // 
+            this.tb_id_product.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_id_product.Location = new System.Drawing.Point(262, 295);
+            this.tb_id_product.Name = "tb_id_product";
+            this.tb_id_product.Size = new System.Drawing.Size(492, 35);
+            this.tb_id_product.TabIndex = 3;
+            // 
+            // tb_name_product
+            // 
+            this.tb_name_product.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_name_product.Location = new System.Drawing.Point(262, 343);
+            this.tb_name_product.Name = "tb_name_product";
+            this.tb_name_product.Size = new System.Drawing.Size(492, 35);
+            this.tb_name_product.TabIndex = 4;
+            // 
+            // tb_price_1kg_rub
+            // 
+            this.tb_price_1kg_rub.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_price_1kg_rub.Location = new System.Drawing.Point(262, 395);
+            this.tb_price_1kg_rub.Name = "tb_price_1kg_rub";
+            this.tb_price_1kg_rub.Size = new System.Drawing.Size(492, 35);
+            this.tb_price_1kg_rub.TabIndex = 5;
+            // 
+            // btn_insert
+            // 
+            this.btn_insert.BackColor = System.Drawing.Color.White;
+            this.btn_insert.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.btn_insert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_insert.Location = new System.Drawing.Point(776, 15);
+            this.btn_insert.Name = "btn_insert";
+            this.btn_insert.Size = new System.Drawing.Size(165, 125);
+            this.btn_insert.TabIndex = 7;
+            this.btn_insert.Text = "добавить";
+            this.btn_insert.UseVisualStyleBackColor = false;
+            this.btn_insert.Click += new System.EventHandler(this.button_insert_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.BackColor = System.Drawing.Color.White;
+            this.btn_delete.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.btn_delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_delete.Location = new System.Drawing.Point(776, 146);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(165, 125);
+            this.btn_delete.TabIndex = 8;
+            this.btn_delete.Text = "удалить";
+            this.btn_delete.UseVisualStyleBackColor = false;
+            this.btn_delete.Click += new System.EventHandler(this.button_delete_Click);
+            // 
+            // btn_update
+            // 
+            this.btn_update.BackColor = System.Drawing.Color.White;
+            this.btn_update.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.btn_update.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_update.Location = new System.Drawing.Point(776, 277);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(165, 125);
+            this.btn_update.TabIndex = 9;
+            this.btn_update.Text = "изменить";
+            this.btn_update.UseVisualStyleBackColor = false;
+            this.btn_update.Click += new System.EventHandler(this.button_update_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(8, 300);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 29);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(8, 346);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 29);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "название";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(8, 398);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(254, 29);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "цена за 1 кг в рублях";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dgv_product);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.tb_id_product);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.tb_name_product);
+            this.panel1.Controls.Add(this.tb_price_1kg_rub);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(9, 15);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(759, 435);
+            this.panel1.TabIndex = 13;
+            // 
+            // execute_info
+            // 
+            this.execute_info.AutoSize = true;
+            this.execute_info.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.execute_info.Location = new System.Drawing.Point(775, 413);
+            this.execute_info.Name = "execute_info";
+            this.execute_info.Size = new System.Drawing.Size(168, 29);
+            this.execute_info.TabIndex = 13;
+            this.execute_info.Text = "запросов нет";
+            // 
+            // Product
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 459);
+            this.Controls.Add(this.execute_info);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_insert);
+            this.Name = "Product";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Продукт";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Product_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_product)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv_product;
+        private System.Windows.Forms.TextBox tb_id_product;
+        private System.Windows.Forms.TextBox tb_name_product;
+        private System.Windows.Forms.TextBox tb_price_1kg_rub;
+        private System.Windows.Forms.Button btn_insert;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label execute_info;
+    }
+}
---
+++ mvc/Product.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Text.RegularExpressions;
+
+namespace projectsamozachet
+{
+    public partial class Product : Form
+    {
+        Facade facade;
+        Form1 form1;
+        MySqlConnection connection;
+        MySqlCommand command;
+
+        public Product(Facade facade, Form1 form1)
+        {
+            InitializeComponent();
+
+            this.facade = facade;
+            this.form1 = form1;
+            this.connection = new MySqlConnection("server=127.0.0.1;username=root;password=root;database=restaurant");
+            connect();
+        }
+
+        void connect()
+        {
+            init_view();
+            data_reuse();
+        }
+
+        void init_view()
+        {
+            this.dgv_product.Columns.Add("id_product", "id_product");
+            this.dgv_product.Columns.Add("name_product", "name_product");
+            this.dgv_product.Columns.Add("price_1kg_rub", "price_1kg_rub");
+        }
+
+        void data_reuse()
+        {
+            this.dgv_product.Rows.Clear();
+            List<List<string>> list_product = this.facade.product_list.get_list_client();
+            foreach (List<string> product in list_product)
+                dgv_product.Rows.Add(product[0], product[1], product[2]);
+        }
+
+        private void Product_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            form1.Show();
+        }
+
+        private void button_insert_Click(object sender, EventArgs e)
+        {
+            int id_product = 1;
+            if (this.dgv_product.Rows.Count > 0)
+                id_product = Convert.ToInt32(this.dgv_product[this.dgv_product.Rows.Count - 1, 0].Value.ToString());
+            string name_product = tb_name_product.Text;
+            int price_1kg_rub = Convert.ToInt32(tb_price_1kg_rub.Text);
+            /*if (el_is_valid(name_product, price_1kg_rub.ToString()) &&
+                (!facade.product_list.get_list_id().Contains(id_product.ToString())))
+            {*/
+                Object[] list = { id_product, name_product, price_1kg_rub };
+                facade.create_product_list(list);
+                string insert_query = "INSERT INTO product(id_product, name_product, price_1kg_rub) " +
+                    $"VALUES ({id_product}, \"{name_product}\", \"{price_1kg_rub}\")";
+                executeMyQuery(insert_query);
+            /*}*/
+            data_reuse();
+        }
+        private void button_update_Click(object sender, EventArgs e)
+        {
+            int id_product = Convert.ToInt32(tb_id_product.Text);
+            string name_product = tb_name_product.Text;
+            int price_1kg_rub = Convert.ToInt32(tb_price_1kg_rub.Text);
+            /*if (el_is_valid(name_product, price_1kg_rub.ToString()) &&
+                (!facade.product_list.get_list_id().Contains(id_product.ToString())))
+            {*/
+                Object[] list = { id_product, name_product, price_1kg_rub };
+                facade.update_product_list(list);
+                string insert_query = string.Format("UPDATE product SET " +
+                    "`id_product`={0}, `name_product`='{1}', `price_1kg_rub`={2} " +
+                    "WHERE `id_product`={0}",
+                    id_product.ToString(), name_product, price_1kg_rub.ToString());
+                executeMyQuery(insert_query);
+            /*}*/
+            data_reuse();
+        }
+
+        private void button_delete_Click(object sender, EventArgs e)
+        {
+            facade.product_list.delete_el(Convert.ToInt32(tb_id_product.Text));
+            string insert_query = string.Format("DELETE FROM `product` WHERE `id_product`={0}",
+                Convert.ToInt32(tb_id_product.Text));
+            executeMyQuery(insert_query);
+            data_reuse();
+        }
+
+        private bool el_is_valid(string name_product, string price_1kg_rub)
+        {
+            bool flag = true;
+            string regex = @"[0-9]{11}";
+            if (!Regex.IsMatch(price_1kg_rub, regex))
+                flag = false;
+            if (name_product == "")
+                flag = false;
+            return flag;
+        }
+
+        public void executeMyQuery(string query)
+        {
+            try
+            {
+                openConnection();
+                command = new MySqlCommand(query, connection);
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    execute_info.ForeColor = System.Drawing.Color.Green;
+                    execute_info.Text = "Выполнено";
+                }
+                else
+                {
+                    execute_info.ForeColor = System.Drawing.Color.Red;
+                    execute_info.Text = "Не выполнено";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                closeConnection();
+            }
+        }
+
+        public void openConnection()
+        {
+            if (connection.State == ConnectionState.Closed)
+                connection.Open();
+        }
+
+        public void closeConnection()
+        {
+            if (connection.State == ConnectionState.Open)
+                connection.Close();
+        }
+    }
+}
